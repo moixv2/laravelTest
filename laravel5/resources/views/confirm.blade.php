9bdb8dbae1d4bc574125567211e215fc0c7f59ef
Quentin Vilcoque <q.vilcoque@gmail.com>
2019-02-17 16:31:26 +0100

    Validation2
    - Création de la vue emailContact
    - Création de la vue contact
    - Création de la vue confirm
    - Création de la requête du formulaire : ContactRequest
    commande : php artisan make:request ContactRequest
    
    - Installation de composer (à cause du changement de wamp)
    - mise à jour de composer

---
+++ laravel5/resources/views/confirm.blade.php
@@ -0,0 +1,15 @@
+@extends('template')
+
+@section('contenu')
+  <br>
+  <div class="col-sm-offset-3 col-sm-6">
+    <div class="panel panel-info">
+      <div class="panl-heading">Contactez-moi</div>
+      <div class="panel-body">
+        Merci. </br>
+        Votre message a éte transmis à l'administrateur du site. </br>
+        Vous recevrez une réponse rapidement.
+      </div>
+    </div>
+  </div>
+@endsection
---
+++ laravel5/resources/views/emailContact.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <<h1> Prise de contact sur mon beau site </h1>
+    <p>Réception d'une prise de contact avec les éléments suivants : </p>
+    <ul>
+      <li><strong>Nom</strong> : {{ $nom }}</li>
+      <li><strong>Email</strong> : {{ $email }}</li>
+      <li><strong>Message</strong> : {{ $texte }}</li>
+    </ul>
+
+
+  </body>
+</html>
